d99ffdc6d2212bfde7f101cf8539b2ed77a44c98
unknown <Shreyasks99@gmail.com>
2019-07-22 14:16:53 +0530

    Added Module 1 Solutions

---
+++ Q1/Q_1.py
@@ -0,0 +1,15 @@
+# Write a program to accept a number and determine whether it is a prime number or not #
+num=int(input("Enter the number"))
+is_prime =True
+if num<2:
+    is_prime=False
+else:
+    for i in range(2,num//2+1):
+        if num % i == 0:
+            is_prime = False
+            break
+if is_prime:
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
+    
