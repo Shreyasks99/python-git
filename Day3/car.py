2823119fe003a93c9acde1cb58da2b4a3ac568a0
unknown <Shreyasks99@gmail.com>
2019-07-24 11:22:27 +0530

    Python

---
+++ Day3/car.py
@@ -0,0 +1,19 @@
+from car1 import Car
+
+bmw = Car("KA013060", 4)
+omni = Car("KA013061",4)
+nano = Car("KA013062",5)
+city = Car("KA013063",6)
+jazz = Car("KA013064",5)
+bmw.start()
+omni.start()
+nano.start()
+bmw.change_gears()
+nano.change_gears()
+
+lst = [bmw,omni,nano,city,jazz]
+
+for car in lst:
+    car.showinfo()
+    c = len(list(filter(lambda x: x.is_started and x.count == 0)))
+    print(c)
---
+++ Day3/car1.py
@@ -0,0 +1,33 @@
+class Car:
+
+    def _init_(self, regno, no_of_gears):
+        self.regno = regno
+        self.no_of_gears = no_of_gears
+        self.is_started = False
+        self.count=0
+
+    def start(self):
+        if self.is_started:
+            print("Car is already started")
+        else:
+            print(f"Car with the regno:{self.regno} is started")
+            self.is_started=True
+
+    def stop(self):
+        if self.is_started:
+            print(f"Car with the regno:{self.regno} is stopped")
+            self.is_started=False
+        else:
+            print("Car is already stopped")
+    def change_gears(self):
+        if self.is_started:
+            if self.count < self.no_of_gears:
+                self.count +=1 
+                print(f"Car with the regno:{self.regno} changed the gears....{self.count}")
+            else:
+                print(f"Reached maximum gears....{self.count}")
+        else:
+            print("Car is stopped and cannot chamge the gears")
+    def showinfo(self):
+        print(f"{self.regno}-Started:{self.is_started} Gears:{self.no_of_gears} Added:{self.count}")
+
